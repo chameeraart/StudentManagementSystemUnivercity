a86b9fe0db897510347331527a98e0fadd3ed8da
chameeraart <madusanka.chameera@gmail.com>
2024-09-10 17:37:25 +0530

    added page

---
+++ StudentManagementSystem/Controllers/StudentExamController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StudentManagementSystem.Controllers
+{
+    public class StudentExamController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("~/Views/Pages/StudentExam.cshtml");
+        }
+    }
+}
